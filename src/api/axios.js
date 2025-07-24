235a0fa7bef611f9959c12c71716c523d2c7534b
kulith_renuja <kulithx175@gmail.com>
2025-07-24 14:34:41 +0530

    axios file changes

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,11 +2,7 @@ import axios from 'axios';
 
 // CRA uses REACT_APP_ prefix for env variables
 const API = axios.create({
-<<<<<<< HEAD
   baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000/api/v1',
-=======
-  baseURL: process.env.REACT_APP_API_URL ||'http://localhost:5000/api/v1',
->>>>>>> 5a83d0b5e4171f1093f02934a81f800ca429f934
 });
 
 API.interceptors.request.use((req) => {
