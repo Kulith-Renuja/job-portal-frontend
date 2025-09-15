c3e25caed2afa8c0c2e0acb0f428c0540163b7d4
kulith_renuja <kulithx175@gmail.com>
2025-09-15 20:41:52 +0530

    feat: Update site branding and metadata for Godayana.lk, including favicon and manifest changes

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,10 +1,20 @@
 import axios from 'axios';
 
 // CRA uses REACT_APP_ prefix for env variables
+
 const API = axios.create({
     baseURL: process.env.REACT_APP_API_URL ,
 });
+/* 
 
+*/
+
+
+/* 
+const API = axios.create({
+    baseURL: 'http://localhost:5000/api/v1',
+});
+*/
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem('token');
   if (token) {
