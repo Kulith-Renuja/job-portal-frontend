e24b7838a144637f66f8eabb24b520a3892abe6a
kulith_renuja <kulithx175@gmail.com>
2026-05-12 23:36:03 +0530

    conflicts resolved - app.js

--- src/App.js
+++ src/App.js
@@ -14,16 +14,12 @@ import Auth from './pages/Auth';
 import Migrations from './pages/Migrations';
 import Countries from './pages/Countries';
 
-<<<<<<< HEAD
 // Importing company components
 import CompanyRegistrationForm from './company/CompanyRegistrationForm';
 import CompanyDashboard from './company/CompanyDashboard';
 
 
 // Importing details pages
-=======
-// Detail pages
->>>>>>> fix-temp
 import JobDetails from './pages/DetailsPages/JobDetails';
 import CourseDetails from './pages/DetailsPages/CourseDetails';
 import MigrationDetails from './pages/DetailsPages/MigrationDetails';
@@ -39,7 +35,6 @@ import ManageMigrations from './admin/ManageMigrations';
 import ManageStories from './admin/ManageStories';
 import ManageUsers from './admin/ManageUsers';
 import ManageCountries from './admin/ManageCountries';
-<<<<<<< HEAD
 import ManageCompanies from './admin/ManageCompanies';
 import AdminAccount from './admin/Account';
 
@@ -47,21 +42,6 @@ import AdminAccount from './admin/Account';
 import ProtectedRoute from './routes/ProtectedRoute';
 import AdminRoute from './routes/AdminRoute';
 import CompanyRoute from './routes/CompanyRoute';
-=======
-import CompanyAdds from './company/CompanyAdds';
-
-// Company
-import CompanyLayout from "./company/CompanyLayout";
-import CompanyDashboard from "./company/CompanyDashboard";
-import CompanyJobs from "./company/CompanyJobs";
-import CompanyProfile from "./company/CompanyProfile";
-import CompanyRoute from "./company/CompanyRoute";
-import CompanyJobPost from "./company/CompanyJobPost";
-
-// Auth protection
-import ProtectedRoute from './components/ProtectedRoute';
-import AdminRoute from './admin/AdminRoute';
->>>>>>> fix-temp
 
 function AppLayout() {
   const location = useLocation();
@@ -73,7 +53,6 @@ function AppLayout() {
   return (
     <>
       {!hideLayout && <Header />}
-<<<<<<< HEAD
       <Routes>
       {/* Public Routes */}
       <Route path="/auth" element={<Auth />} />
@@ -129,40 +108,6 @@ function AppLayout() {
       
       
         {/* Admin Routes (protected) */}
-=======
-
-      <Routes>
-        {/* Public routes */}
-        <Route path="/auth" element={<Auth />} />
-        <Route path="/" element={<Home />} />
-        <Route path="/categories" element={<Categories />} />
-        <Route path="/visa" element={<Migrations />} />
-        <Route path="/countries" element={<Countries />} />
-        <Route path="/jobs" element={<Jobs />} />
-        <Route path="/stories" element={<Stories />} />
-        <Route path="/courses" element={<Courses />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/account" element={<ProtectedRoute><Account /></ProtectedRoute>} />
-
-        {/* Detail routes */}
-        <Route path="/jobs/:id" element={<JobDetails />} />
-        <Route path="/courses/:id" element={<CourseDetails />} />
-        <Route path="/migration/:id" element={<MigrationDetails />} />
-        <Route path="/stories/:id" element={<StoryDetails />} />
-        <Route path="/countries/:id" element={<CountryDetails />} />
-
-        {/* Admin routes */}
-        <Route path="/admin/dashboard" element={<AdminRoute><AdminLayout><Dashboard /></AdminLayout></AdminRoute>} />
-        <Route path="/admin/jobs" element={<AdminRoute><AdminLayout><ManageJobs /></AdminLayout></AdminRoute>} />
-        <Route path="/admin/courses" element={<AdminRoute><AdminLayout><ManageCourses /></AdminLayout></AdminRoute>} />
-        <Route path="/admin/visa" element={<AdminRoute><AdminLayout><ManageMigrations /></AdminLayout></AdminRoute>} />
-        <Route path="/admin/stories" element={<AdminRoute><AdminLayout><ManageStories /></AdminLayout></AdminRoute>} />
-        <Route path="/admin/users" element={<AdminRoute><AdminLayout><ManageUsers /></AdminLayout></AdminRoute>} />
-        <Route path="/admin/countries" element={<AdminRoute><AdminLayout><ManageCountries /></AdminLayout></AdminRoute>} />
-        <Route path="/admin/account" element={<AdminLayout><Account /></AdminLayout>} />
-
-        {/* Company routes */}
->>>>>>> fix-temp
         <Route
           path="/company"
           element={
@@ -170,7 +115,6 @@ function AppLayout() {
               <CompanyLayout />
             </CompanyRoute>
           }
-<<<<<<< HEAD
         />
         <Route
           path="/admin/jobs"
@@ -251,15 +195,6 @@ function AppLayout() {
           }
         />
               
-=======
-        >
-          <Route path="dashboard" element={<CompanyDashboard />} />
-          <Route path="jobs" element={<CompanyJobs />} />
-          <Route path="profile" element={<CompanyProfile />} />
-          <Route path="adds" element={<CompanyAdds />} />
-          <Route path="post-job" element={<CompanyJobPost />} />
-        </Route>
->>>>>>> fix-temp
       </Routes>
 
       {!hideLayout && <Footer />}
